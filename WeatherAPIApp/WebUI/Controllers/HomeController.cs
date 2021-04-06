4e371d3d4aac052ef521213a04dc5d53be2d162e
urbanurbz <55708962+urbanurbz@users.noreply.github.com>
2021-04-06 15:37:38 -0400

    added a wepage MVC project to the WeatherApp, still needs some work (and styling) but it is functional.

---
+++ WeatherAPIApp/WebUI/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WeatherAPILib;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            //APIHelper.InitializeClient(); // this does work here, however it might be better if placed in BundleConfig.cs (?)
+
+            //List<CitySearchModel> search = await CitySearchProcessor.LoadSearchInformation(ViewBag.City);
+
+            //WeatherDetailsModel city = await WeatherProcessor.LoadWeatherInformation(search[0].Woeid);
+
+            //List<WeatherModel> forecast = city.Consolidated_Weather;
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> CityForm(string data)
+        {
+            List<CitySearchModel> search = await CitySearchProcessor.LoadSearchInformation(data);
+            List<WeatherModel> forecast = new List<WeatherModel>();
+
+            try
+            {
+                WeatherDetailsModel city = await WeatherProcessor.LoadWeatherInformation(search[0].Woeid);
+                forecast = city.Consolidated_Weather;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return View("Index", forecast);
+            }
+            
+
+            
+
+            return View("Index", forecast);
+        }
+    }
+}
---
+++ WeatherAPIApp/WebUI/Models/WeatherFormData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class WeatherFormData
+    {
+        public string CityName { get; set; }
+    }
+}
